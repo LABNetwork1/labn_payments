e80749b8264b98799a800c170a185ea4a3b19c22
LAB Network <106167115+LABNetwork1@users.noreply.github.com>
2022-07-09 14:30:05 +0100

    Add files via upload

--- client/main.lua
+++ client/main.lua
@@ -33,18 +33,18 @@ RegisterNetEvent("labn_payments:client:showMenuPayments", function()
         table.insert(elements, {
             title = "📄 Invoices",
             description = "View My Unpaid Invoices",
-            event = "labn_payments:client:ShowInvoicesMenu"
+            event = "labn_payments:client:ShowInvoicesStatusMenu"
         })
         table.insert(elements, {
             title = "📑 Fines",
             description = "View My Unpaid Fines",
-            event = "labn_payments:client:ShowFinesMenu"
+            event = "labn_payments:client:ShowFinesStatusMenu"
         })
         if Config.SocietyEmergencys and isAllowed then
             table.insert(elements, {
-                title = "📑 Create Fine",
+                title = "📑 Create Custom Fine",
                 description = "Create Fine to the Nearest Civilian",
-                event = "labn_payments:client:CreateFine"
+                event = "labn_payments:client:CreateCustomFine"
             })
             table.insert(elements, {
                 title = "📑 Check Fines",
@@ -68,6 +68,42 @@ RegisterNetEvent("labn_payments:client:showMenuPayments", function()
     end
 end)
 
+RegisterNetEvent("labn_payments:client:ShowInvoicesStatusMenu", function()
+    if ESX.PlayerLoaded and not isDead then
+        local elements = {}
+        table.insert(elements, {
+            title = "📄 Unpaid Invoices",
+            description = "View My Unpaid Invoices",
+            event = "labn_payments:client:ShowInvoicesMenu"
+        })
+        table.insert(elements, {
+            title = "📄 Paid Invoices",
+            description = "View My Paid Invoices",
+            event = "labn_payments:client:ShowInvoicesPaidMenu"
+        })
+        lib.registerContext({id = "show_invoices_status_menu", title = "📄 Invoices", menu = "show_payments_menu", options = elements})
+        lib.showContext("show_invoices_status_menu")
+    end
+end)
+
+RegisterNetEvent("labn_payments:client:ShowFinesStatusMenu", function()
+    if ESX.PlayerLoaded and not isDead then
+        local elements = {}
+        table.insert(elements, {
+            title = "📑 Unpaid Fines",
+            description = "View My Unpaid Fines",
+            event = "labn_payments:client:ShowFinesMenu"
+        })
+        table.insert(elements, {
+            title = "📑 Paid Fines",
+            description = "View My Paid Fines",
+            event = "labn_payments:client:ShowFinesPaidMenu"
+        })
+        lib.registerContext({id = "show_fines_status_menu", title = "📑 Fines", menu = "show_payments_menu", options = elements})
+        lib.showContext("show_fines_status_menu")
+    end
+end)
+
 RegisterNetEvent("labn_payments:client:ShowInvoicesMenu", function()
     if ESX.PlayerLoaded and not isDead then
         ESX.TriggerServerCallback("labn_payments:server:getInvoices", function(invoices)
@@ -79,11 +115,40 @@ RegisterNetEvent("labn_payments:client:ShowInvoicesMenu", function()
                         title = ""..v.label.."",
                         description = "Invoice Amount: $"..ESX.Math.GroupDigits(v.amount).."",
                         event = "labn_payments:client:payInvoices",
+                        metadata = {
+                            {label = "Send Date", value = v.send_date},
+                        },
                         args = {invoiceId = v.id}
                     })
                 end
-                lib.registerContext({id = "show_invoices_menu", title = "Unpaid Invoices", menu = "show_payments_menu", options = elements})
-                lib.showContext("show_invoices_menu")
+                lib.registerContext({id = "show_unpaid_invoices_menu", title = "Unpaid Invoices", menu = "show_invoices_status_menu", options = elements})
+                lib.showContext("show_unpaid_invoices_menu")
+            else
+                lib.notify({description = "You Have No Invoice!", type = "inform"})
+            end
+        end)
+    end
+end)
+
+RegisterNetEvent("labn_payments:client:ShowInvoicesPaidMenu", function()
+    if ESX.PlayerLoaded and not isDead then
+        ESX.TriggerServerCallback("labn_payments:server:getInvoicesPaid", function(invoices)
+            if #invoices > 0 then
+                ESX.UI.Menu.CloseAll()
+                local elements = {}
+                for k, v in ipairs(invoices) do
+                    table.insert(elements, {
+                        title = ""..v.label.."",
+                        description = "Invoice Amount: $"..ESX.Math.GroupDigits(v.amount).."",
+                        metadata = {
+                            {label = "Send Date", value = v.send_date},
+                            {label = "Paid Date", value = v.paid_date},
+                        },
+                        args = {invoiceId = v.id}
+                    })
+                end
+                lib.registerContext({id = "show_paid_invoices_menu", title = "Paid Invoices", menu = "show_invoices_status_menu", options = elements})
+                lib.showContext("show_paid_invoices_menu")
             else
                 lib.notify({description = "You Have No Invoice!", type = "inform"})
             end
@@ -99,7 +164,7 @@ RegisterNetEvent("labn_payments:client:CreateInvoice", function()
             if input then
                 local Invoicelabel = input[1]
                 local InvoiceAmount = tonumber(input[2])
-                if InvoiceAmount == 0 then
+                if InvoiceAmount <= 0 then
                     return lib.notify({description = "Invalid Amount!", type = "error"})
                 end
                 TriggerServerEvent("labn_payments:server:sendInvoice", GetPlayerServerId(closestPlayer), Invoicelabel, InvoiceAmount)
@@ -138,6 +203,9 @@ RegisterNetEvent("labn_payments:client:ShowInvoicesTargetMenu", function()
                         table.insert(elements, {
                             title = ""..invoice.label.."",
                             description = "Invoice Amount: $"..ESX.Math.GroupDigits(invoice.amount).."",
+                            metadata = {
+                                {label = "Send Date", value = invoice.send_date},
+                            },
                             args = {invoiceId = invoice.id}
                         })
                     end
@@ -147,6 +215,8 @@ RegisterNetEvent("labn_payments:client:ShowInvoicesTargetMenu", function()
                     lib.notify({description = "This Civilian has no Invoice!", type = "inform"})
                 end
             end, GetPlayerServerId(closestPlayer))
+        else
+            lib.notify({description = "No Civilians Nearby!", type = "error"})
         end
     end
 end)
@@ -162,10 +232,13 @@ RegisterNetEvent("labn_payments:client:ShowFinesMenu", function()
                         title = ""..v.label.."",
                         description = "Fine Amount: $"..ESX.Math.GroupDigits(v.amount).."",
                         event = "labn_payments:client:payFines",
+                        metadata = {
+                            {label = "Send Date", value = v.send_date},
+                        },
                         args = {fineId = v.id}
                     })
                 end
-                lib.registerContext({id = "show_fines_menu", title = "Unpaid Fines", menu = "show_payments_menu", options = elements})
+                lib.registerContext({id = "show_fines_menu", title = "Unpaid Fines", menu = "show_fines_status_menu", options = elements})
                 lib.showContext("show_fines_menu")
             else
                 lib.notify({description = "You Have No Fines!", type = "inform"})
@@ -174,7 +247,33 @@ RegisterNetEvent("labn_payments:client:ShowFinesMenu", function()
     end
 end)
 
-RegisterNetEvent("labn_payments:client:CreateFine", function()
+RegisterNetEvent("labn_payments:client:ShowFinesPaidMenu", function()
+    if ESX.PlayerLoaded and not isDead then
+        ESX.TriggerServerCallback("labn_payments:server:getFinesPaid", function(fines)
+            if #fines > 0 then
+                ESX.UI.Menu.CloseAll()
+                local elements = {}
+                for k, v in ipairs(fines) do
+                    table.insert(elements, {
+                        title = ""..v.label.."",
+                        description = "Fine Amount: $"..ESX.Math.GroupDigits(v.amount).."",
+                        metadata = {
+                            {label = "Send Date", value = v.send_date},
+                            {label = "Paid Date", value = v.paid_date},
+                        },
+                        args = {fineId = v.id}
+                    })
+                end
+                lib.registerContext({id = "show_paid_fines_menu", title = "Paid Fines", menu = "show_fines_status_menu", options = elements})
+                lib.showContext("show_paid_fines_menu")
+            else
+                lib.notify({description = "You Have No Fines!", type = "inform"})
+            end
+        end)
+    end
+end)
+
+RegisterNetEvent("labn_payments:client:CreateCustomFine", function()
     if ESX.PlayerLoaded and not isDead then
         local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
 		if closestPlayer ~= -1 and closestDistance <= 3.0 then
@@ -221,6 +320,9 @@ RegisterNetEvent("labn_payments:client:ShowFinesTargetMenu", function()
                         table.insert(elements, {
                             title = ""..fine.label.."",
                             description = "Fine Amount: $"..ESX.Math.GroupDigits(fine.amount).."",
+                            metadata = {
+                                {label = "Send Date", value = fine.send_date},
+                            },
                             args = {fineId = fine.id}
                         })
                     end
@@ -230,6 +332,8 @@ RegisterNetEvent("labn_payments:client:ShowFinesTargetMenu", function()
                     lib.notify({description = "This Civilian has no Fines!", type = "inform"})
                 end
             end, GetPlayerServerId(closestPlayer))
+        else
+            lib.notify({description = "No Civilians Nearby!", type = "error"})
         end
     end
 end)
--- server/main.lua
+++ server/main.lua
@@ -3,7 +3,7 @@ AddEventHandler("labn_payments:server:sendFine", function(playerId, label, amoun
 	local xPlayer = ESX.GetPlayerFromId(source)
 	local xTarget = ESX.GetPlayerFromId(playerId)
 	if xTarget then
-		MySQL.insert("INSERT INTO labn_payments (identifier, label, amount, status, type) VALUES (?, ?, ?, ?, ?)", {xTarget.identifier, label, amount, "unpaid", "multa"}, function(rowsChanged)
+		MySQL.insert("INSERT INTO labn_payments (identifier, label, amount, status, type, send_date) VALUES (?, ?, ?, ?, ?, ?)", {xTarget.identifier, label, amount, "unpaid", "multa", os.date("%m-%m-%Y %H:%M:%S")}, function(rowsChanged)
 			TriggerClientEvent("ox_lib:notify", xTarget.source, {description = "You received a fine", type = "inform"})
 		end)
 	end
@@ -14,7 +14,7 @@ AddEventHandler("labn_payments:server:sendInvoice", function(playerId, label, am
 	local xPlayer = ESX.GetPlayerFromId(source)
 	local xTarget = ESX.GetPlayerFromId(playerId)
 	if xTarget then
-		MySQL.insert("INSERT INTO labn_payments (identifier, label, amount, status, type) VALUES (?, ?, ?, ?, ?)", {xTarget.identifier, label, amount, "unpaid", "fatura"}, function(rowsChanged)
+		MySQL.insert("INSERT INTO labn_payments (identifier, label, amount, status, type, send_date) VALUES (?, ?, ?, ?, ?, ?)", {xTarget.identifier, label, amount, "unpaid", "fatura", os.date("%m-%m-%Y %H:%M:%S")}, function(rowsChanged)
 			TriggerClientEvent("ox_lib:notify", xTarget.source, {description = "You received an Invoice", type = "inform"})
 		end)
 	end
@@ -22,14 +22,28 @@ end)
 
 ESX.RegisterServerCallback("labn_payments:server:getFines", function(source, cb)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	MySQL.query("SELECT amount, id, status, type, label FROM labn_payments WHERE identifier = ? AND status = 'unpaid' AND type = 'multa'", {xPlayer.identifier}, function(result)
+	MySQL.query("SELECT * FROM labn_payments WHERE identifier = ? AND status = 'unpaid' AND type = 'multa'", {xPlayer.identifier}, function(result)
 		cb(result)
 	end)
 end)
 
 ESX.RegisterServerCallback("labn_payments:server:getInvoices", function(source, cb)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	MySQL.query("SELECT amount, id, status, type, label FROM labn_payments WHERE identifier = ? AND status = 'unpaid' AND type = 'fatura'", {xPlayer.identifier}, function(result)
+	MySQL.query("SELECT * FROM labn_payments WHERE identifier = ? AND status = 'unpaid' AND type = 'fatura'", {xPlayer.identifier}, function(result)
+		cb(result)
+	end)
+end)
+
+ESX.RegisterServerCallback("labn_payments:server:getFinesPaid", function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	MySQL.query("SELECT * FROM labn_payments WHERE identifier = ? AND status = 'paid' AND type = 'multa'", {xPlayer.identifier}, function(result)
+		cb(result)
+	end)
+end)
+
+ESX.RegisterServerCallback("labn_payments:server:getInvoicesPaid", function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	MySQL.query("SELECT * FROM labn_payments WHERE identifier = ? AND status = 'paid' AND type = 'fatura'", {xPlayer.identifier}, function(result)
 		cb(result)
 	end)
 end)
@@ -37,7 +51,7 @@ end)
 ESX.RegisterServerCallback("labn_payments:server:getTargetFines", function(source, cb, target)
 	local xPlayer = ESX.GetPlayerFromId(target)
 	if xPlayer then
-		MySQL.query("SELECT amount, id, status, type, label FROM labn_payments WHERE identifier = ? AND status = 'unpaid' AND type = 'multa'", {xPlayer.identifier}, function(result)
+		MySQL.query("SELECT * FROM labn_payments WHERE identifier = ? AND status = 'unpaid' AND type = 'multa'", {xPlayer.identifier}, function(result)
 			cb(result)
 		end)
 	else
@@ -48,7 +62,29 @@ end)
 ESX.RegisterServerCallback("labn_payments:server:getTargetInvoices", function(source, cb, target)
 	local xPlayer = ESX.GetPlayerFromId(target)
 	if xPlayer then
-		MySQL.query("SELECT amount, id, status, type, label FROM labn_payments WHERE identifier = ? AND status = 'unpaid' AND type = 'fatura'", {xPlayer.identifier}, function(result)
+		MySQL.query("SELECT * FROM labn_payments WHERE identifier = ? AND status = 'unpaid' AND type = 'fatura'", {xPlayer.identifier}, function(result)
+			cb(result)
+		end)
+	else
+		cb({})
+	end
+end)
+
+ESX.RegisterServerCallback("labn_payments:server:getTargetFinesPaid", function(source, cb, target)
+	local xPlayer = ESX.GetPlayerFromId(target)
+	if xPlayer then
+		MySQL.query("SELECT * FROM labn_payments WHERE identifier = ? AND status = 'paid' AND type = 'multa'", {xPlayer.identifier}, function(result)
+			cb(result)
+		end)
+	else
+		cb({})
+	end
+end)
+
+ESX.RegisterServerCallback("labn_payments:server:getTargetInvoicesPaid", function(source, cb, target)
+	local xPlayer = ESX.GetPlayerFromId(target)
+	if xPlayer then
+		MySQL.query("SELECT * FROM labn_payments WHERE identifier = ? AND status = 'paid' AND type = 'fatura'", {xPlayer.identifier}, function(result)
 			cb(result)
 		end)
 	else
@@ -58,17 +94,17 @@ end)
 
 ESX.RegisterServerCallback("labn_payments:server:payFine", function(source, cb, fineId)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	MySQL.single("SELECT amount FROM labn_payments WHERE id = ?", {fineId}, function(result)
+	MySQL.single("SELECT * FROM labn_payments WHERE id = ?", {fineId}, function(result)
 		if result then
 			local amount = result.amount
 			if xPlayer.getMoney() >= amount then
-				MySQL.update("UPDATE labn_payments SET status = ? WHERE id = ?", {"paid", fineId}, function(affectedRows)
+				MySQL.update("UPDATE labn_payments SET status = ?, paid_date = ? WHERE id = ?", {"paid", os.date("%m-%m-%Y %H:%M:%S"), fineId}, function(affectedRows)
 					xPlayer.removeMoney(amount)
 					TriggerClientEvent("ox_lib:notify", xPlayer.source, {description = "Tu Pagas-te a Multa no Valor de $"..ESX.Math.GroupDigits(amount).."", type = "success"})
 					cb()
 				end)
 			elseif xPlayer.getAccount("bank").money >= amount then
-				MySQL.update("UPDATE labn_payments SET status = ? WHERE id = ?", {"paid", fineId}, function(affectedRows)
+				MySQL.update("UPDATE labn_payments SET status = ?, paid_date = ? WHERE id = ?", {"paid", os.date("%m-%m-%Y %H:%M:%S"), fineId}, function(affectedRows)
 					xPlayer.removeAccountMoney("bank", amount)
 					TriggerClientEvent("ox_lib:notify", xPlayer.source, {description = "Tu Pagas-te a Multa no Valor de $"..ESX.Math.GroupDigits(amount).."", type = "success"})
 					cb()
@@ -83,17 +119,17 @@ end)
 
 ESX.RegisterServerCallback("labn_payments:server:payInvoice", function(source, cb, invoiceId)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	MySQL.single("SELECT amount FROM labn_payments WHERE id = ?", {invoiceId}, function(result)
+	MySQL.single("SELECT * FROM labn_payments WHERE id = ?", {invoiceId}, function(result)
 		if result then
 			local amount = result.amount
 			if xPlayer.getMoney() >= amount then
-				MySQL.update("UPDATE labn_payments SET status = ? WHERE id = ?", {"paid", invoiceId}, function(affectedRows)
+				MySQL.update("UPDATE labn_payments SET status = ?, paid_date = ? WHERE id = ?", {"paid", os.date("%m-%m-%Y %H:%M:%S"), invoiceId}, function(affectedRows)
 					xPlayer.removeMoney(amount)
 					TriggerClientEvent("ox_lib:notify", xPlayer.source, {description = "Tu Pagas-te a Fatura no Valor de $"..ESX.Math.GroupDigits(amount).."", type = "success"})
 					cb()
 				end)
 			elseif xPlayer.getAccount("bank").money >= amount then
-				MySQL.update("UPDATE labn_payments SET status = ? WHERE id = ?", {"paid", invoiceId}, function(affectedRows)
+				MySQL.update("UPDATE labn_payments SET status = ?, paid_date = ? WHERE id = ?", {"paid", os.date("%m-%m-%Y %H:%M:%S"), invoiceId}, function(affectedRows)
 					xPlayer.removeAccountMoney("bank", amount)
 					TriggerClientEvent("ox_lib:notify", xPlayer.source, {description = "Tu Pagas-te a Fatura no Valor de $"..ESX.Math.GroupDigits(amount).."", type = "success"})
 					cb()
